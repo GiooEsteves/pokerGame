f314d08b2c411767ae5a38f3efea6a7ffae39074
Giovanna Esteves <gio_esteves@hotmail.com>
2024-08-19 23:55:16 -0300

    regra de negocio

---
+++ models/card.py
@@ -0,0 +1,8 @@
+class Card():
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def biggerCard():
+        pass
+    
---
+++ models/deck.py
@@ -0,0 +1,14 @@
+class Deck():
+    # todos os valores de cartas 
+    suit = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
+    value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
+    def __init__(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def distributeCards(self):
+        pass
+    
